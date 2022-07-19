3fbef0f43f15aa491d70f956109211257edce336
minju-kim98 <ducco705@snu.ac.kr>
2022-07-19 17:53:44 +0900

    evaluator test _ fixed

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -27,9 +27,8 @@ func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
-	env := object.NewEnvironment()
 
-	return Eval(program, env)
+	return Eval(program)
 }
 
 func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
