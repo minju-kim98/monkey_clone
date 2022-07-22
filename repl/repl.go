122fbbfebaf225a3a6e43d774bbdc2fa574010e4
minju-kim98 <ducco705@snu.ac.kr>
2022-07-22 14:33:31 +0900

    environment and binding

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/minju-kim98/monkey_clone/evaluator"
 	"github.com/minju-kim98/monkey_clone/lexer"
+	"github.com/minju-kim98/monkey_clone/object"
 	"github.com/minju-kim98/monkey_clone/parser"
 )
 
@@ -26,6 +27,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Printf(PROMPT)
@@ -44,7 +46,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
